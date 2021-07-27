da48820cf3b96816663d651a56d862b0a57937f6
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-07-27 16:06:38 -0600

    Use Pydantic

--- panelify/canvas.py
+++ panelify/canvas.py
@@ -1,21 +1,25 @@
-import dataclasses
 import typing
 
 import panel as pn
+import pydantic
 
 from .dashboard import Dashboard
 
 
-@dataclasses.dataclass
+@pydantic.dataclasses.dataclass
 class Canvas:
+    """
+    A Canvas is a collection of Dashboards.
+    """
+
     objs: typing.Dict[str, Dashboard]
 
-    def __post_init__(self):
-        tabs = []
-        for item, dash in self.objs.items():
-            tabs.append((item, dash))
-
+    def __post_init_post_parse__(self):
+        tabs = [(item, dash) for item, dash in self.objs.items()]
         self._canvas = pn.Tabs(*tabs, tabs_location='above')
 
     def show(self):
+        """
+        Show the canvas.
+        """
         return pn.Row(self._canvas)
